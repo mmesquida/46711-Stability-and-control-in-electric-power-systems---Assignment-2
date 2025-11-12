fade6fd79838d5ed9cb014c88e1f40be8e2d897b
JLindholm2203 <jonas.lindholm@rwth-aachen.de>
2025-11-12 16:08:55 +0100

    calculate eigenvalues and pmatrix in 2.2

--- Assignment_helpfunctions_part_II/Identify_PSS_locations_help.py
+++ Assignment_helpfunctions_part_II/Identify_PSS_locations_help.py
@@ -4,6 +4,9 @@ import scipy.io as sio
 import numpy as np
 import os
 
+from compute_results import get_eigenvalues, get_P_matrix
+from print_results import print_eigenvalues, print_P_matrix
+
 
 """ Load System Data """
 current_path = os.getcwd()
@@ -20,6 +23,15 @@ strsps = sys_data['strsps']
 statename = sys_data['StateName']
 
 """ Determine Eigenvalues and calculate Eigenvectors and participation matrix """
+# get right eigenvalues, damped frequency and damping ratio
+lambda_A, f_d, zeta = get_eigenvalues(A)
+
+# get participation matrix, right and left eigenvector matrix
+P, Phi, Psi = get_P_matrix(A)
+
+# print participation matrix
+row_headers = ['ΔδG1', 'ΔωG1', 'Δψ/f,G1', 'Δψ/kd,G1', 'Δψ/kq1,G1', 'Δψ/kq2,G1','Δv/m,exc,G1','ΔδG2', 'ΔωG2', 'Δψ/f,G2', 'Δψ/kd,G2', 'Δψ/kq1,G2', 'Δψ/kq2,G2','Δv/m,exc,G2','ΔδG3', 'ΔωG3', 'Δψ/f,G3', 'Δψ/kd,G3', 'Δψ/kq1,G3', 'Δψ/kq2,G3','Δv/m,exc,G3','ΔδG4', 'ΔωG4', 'Δψ/f,G4', 'Δψ/kd,G4', 'Δψ/kq1,G4', 'Δψ/kq2,G4','Δv/m,exc,G4']
+print_P_matrix(P, row_headers)
 
 """ Find the location of relevant indices in the original system """
 
